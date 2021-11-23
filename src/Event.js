6c89b55a2ed668f77c7ea2586bf5aa640cbfbe60
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-22 21:13:45 -0800

    Better Template

--- src/Event.js
+++ src/Event.js
@@ -3,9 +3,14 @@ import './styles/Events.css';
 function Event(props) {
     // console.log("EVENT-DATA", props.date, props.desc)
     return (
-        <div className={(props.index % 2 === 0) ? "container right" : "container left"}>
-            <h2>{props.date}</h2>
-            <p>{props.desc}</p>
+        <div className={(props.index % 2 === 0) ? "container left" : "container right"}>
+            <div className={"date"}>{props.date}</div>
+            <i className={"icon"}></i>
+            <div className={"content"}>
+                <h2>{props.desc}</h2>
+                <p>{props.comment}</p>
+            </div>
+
         </div>
     );
 }
