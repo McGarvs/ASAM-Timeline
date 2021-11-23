ee37de451f07f8362f9927f5a1368929d7145206
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-11-22 21:46:32 -0800

    Fixed apostraphe issue

--- src/convert/txtTojson.py
+++ src/convert/txtTojson.py
@@ -1,7 +1,8 @@
 import json
+import codecs
 
 def notEmpty(l):
-    return (l != [""])
+    return (l != ["\r"])
 
 def convert(mylist):
     dictList = []
@@ -11,15 +12,15 @@ def convert(mylist):
     return dictList
 
 if __name__ == "__main__":
-    with open("info.txt", "r") as my_input:
-        lines = my_input.readlines()
-        info = list(map(lambda l: (str(l.replace("\n", ""))).split(":"), lines))
-        info = list(filter(lambda x: notEmpty(x), info))
-        for i in range(len(info)):
-            info[i] = tuple([info[i][0], info[i][1][1:]])
-        newinfo = convert(info)
-        jsonString = json.dumps(newinfo)
-        jsonFile = open("data.json", "w")
-        jsonFile.write(jsonString)
-        jsonFile.close()
+    my_input =  codecs.open("info.txt", "r", "utf-8")
+    lines = my_input.readlines()
+    info = list(map(lambda l: (str(l.replace("\n", ""))).split(":"), lines))
+    info = list(filter(lambda l: (l != ["\r"]), info))
+    for i in range(len(info)):
+        info[i] = tuple([info[i][0], info[i][1][1:]])
+    newinfo = convert(info)
+    jsonString = json.dumps(newinfo)
+    jsonFile = codecs.open("data.json", "w", "utf-8")
+    jsonFile.write(jsonString)
+    jsonFile.close()
     my_input.close()
